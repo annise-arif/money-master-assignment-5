d4828bfa8514d7062f43d326902b72996812ac94
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-02-17 23:22:48 +0600

    added js calculation

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 document.getElementById('calculate-btn').addEventListener('click', function(){
     const incomeValue = document.getElementById('income-value');
     const incomeText = parseFloat(incomeValue.value);
-
+    if(isNaN(incomeText) ){
+        alert("income text is not a number");
+    }
     // total expenses section
     function getInputValue( id,){
         const input = document.getElementById(id);
         const amount = parseFloat(input.value);
-        return amount;
+        if(isNaN(amount)){
+            alert("Is not a number Pleaze Enter the Valid Number");
+        }
+        return amount;       
     }
 
     
     const foodAmount = getInputValue('food');
     const rentAmount = getInputValue('rent');
-    const clothesAmount = getInputValue('clothes')
+    const clothesAmount = getInputValue('clothes');
+    allExpensesAmount = foodAmount + rentAmount + clothesAmount;
 
-    const allExpnsesAmount = foodAmount + rentAmount + clothesAmount;
     //update balance
+  
     const totalExpenses = document.getElementById('total-expenses');
-    totalExpenses.innerText = allExpnsesAmount;
+    totalExpenses.innerText = allExpensesAmount;
     const balance = document.getElementById('current-balance');
-    balance.innerText = incomeText - allExpnsesAmount;
+    balance.innerText = incomeText - allExpensesAmount;
     
-    
-
-
+ 
 });
