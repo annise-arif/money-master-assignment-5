1eb95b8dadaca569476a52a6388ec64934bf5476
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-02-18 17:39:49 +0600

    added new feature

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 document.getElementById('calculate-btn').addEventListener('click', function(){
-    const incomeValue = document.getElementById('income-value');
-    const incomeText = parseFloat(incomeValue.value);
-    if(isNaN(incomeText) ){
+    const incomeText = document.getElementById('income-value');
+    const incomeValue = parseFloat(incomeText.value);
+    if(isNaN(incomeValue)){
         alert("income is not a number");
-    }else if(!isNaN(incomeText) && incomeText < 0){
+    }else if(!isNaN(incomeValue) && incomeValue < 0){
         alert("Please Enter a Positive Number");
     }
     // total expenses section
@@ -29,15 +29,26 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const totalExpenses = document.getElementById('total-expenses');
     totalExpenses.innerText = allExpensesAmount;
     const balance = document.getElementById('current-balance');
-    balance.innerText = incomeText - allExpensesAmount;
-    
-    //extra saving section
-
-    // document.getElementById('calculate-btn').addEventListener('click', function {
-    //     const savingAmount = document.getElementById('saving-amount').innerText;
-    //     savingAmount =
-    // });
-
-    // I haven't enough time to do the extra work
+    balance.innerText = incomeValue - allExpensesAmount;   
  
 });
+
+ //extra saving section
+
+ document.getElementById('save-btn').addEventListener('click', function(){
+    const incomeText = document.getElementById('income-value');
+    const incomeValue = parseFloat(incomeText.value);
+
+    const savingField = document.getElementById('saving-amount');
+    // const savingValue = savingField.innerText;
+
+    const saveInput = document.getElementById('saving-input');
+    const saveValue = parseFloat(saveInput.value);
+    
+    const savingAmount = incomeValue / saveValue;
+    savingField.innerText = savingAmount;
+
+    // const remainingField = document.getElementById('remaining-balance');
+   
+
+})
